9bbad46086e5146bf8eb3a8a6606d92bf1134eb7
Mateusz Piwnicki <matpiw@gmail.com>
2018-12-05 12:12:06 +0100

    SBC theme init

--- core/filters/price/index.js
+++ core/filters/price/index.js
@@ -11,8 +11,8 @@ export function price (value) {
   const formattedVal = parseFloat(value).toFixed(2)
   const storeView = currentStoreView()
   if (value >= 0) {
-    return storeView.i18n.currencySign + formattedVal
+    return storeView.i18n.currencySign + ' ' + formattedVal
   } else {
-    return '-' + storeView.i18n.currencySign + Math.abs(formattedVal)
+    return '-' + storeView.i18n.currencySign + ' ' + Math.abs(formattedVal)
   }
 }
